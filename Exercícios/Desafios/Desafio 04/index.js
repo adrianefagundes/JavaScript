2223a136c95622581b1df020acd13fd703d20167
Adriane Fagundes <adriane.cristina.fagundes@gmail.com>
2022-06-23 16:09:02 -0300

    Add: desafio tabuada
    Finalização do desafio. Código em JS e melhoria no CSS.

---
+++ Exercícios/Desafios/Desafio 04/index.js
@@ -0,0 +1,18 @@
+function tabuada() {
+    let num = document.getElementById('numbertxt')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        windod.alert('Valor não indicado.')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML='' // Para limpar o Select e não adicionar outra tabuada embaixo da outra.
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
