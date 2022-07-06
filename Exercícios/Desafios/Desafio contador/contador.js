f0aa8d5d35cec46882ede0f97c893d603970d2d8
Adriane Fagundes <adriane.cristina.fagundes@gmail.com>
2022-07-06 17:06:34 -0300

    Style: números negativos ficam vermelhos
    Utilização de DOM.

--- Exercícios/Desafios/Desafio contador/contador.js
+++ Exercícios/Desafios/Desafio contador/contador.js
@@ -4,9 +4,16 @@ let numero = 0
 function mais() {
     numero = numero + 1;
     numerodocumento.innerHTML = numero;
+    if (numero >= 0) {
+        numerodocumento.style.color = "black";
+    }
 }
 
 function menos() {
     numero = numero - 1;
     numerodocumento.innerHTML = numero;
+    if (numero < 0) {
+        numerodocumento.style.color = "red";
+    }
 }
+
